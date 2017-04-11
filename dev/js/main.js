f0eb9938e19a320e4f561acbe1f750ba419ecd81
MaryPoddubnaya <mary.poddubnaya@teamdev.com>
2017-04-11 18:47:13 +0300

    Added images. Created animation

--- dev/js/main.js
+++ dev/js/main.js
@@ -15,5 +15,23 @@ $(document).ready(function() {
         function getCurrentScroll() {
             return window.pageYOffset || document.documentElement.scrollTop;
         }
+});
+
+
+// Get the height of the header
+var headerHeight = $("header").height();
+
+$(".flex-nav .flex-item-list").find("a").bind("click", function(e) {
+
+    e.preventDefault();
+    var target = $(this).attr("href"); //Get the target
+    var scrollToPosition = $(target).offset().top - headerHeight;
+
+    $("html, body").animate({ 'scrollTop': scrollToPosition }, 900, function(){
+        window.location.hash = "" + target;
+        // This hash change will jump the page to the top of the div with the same id
+        // so we need to force the page to back to the end of the animation
+        $("html, body").animate({ 'scrollTop': scrollToPosition }, 0);
+    });
 
 });
