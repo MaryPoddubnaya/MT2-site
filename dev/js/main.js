0ed7160e6f8f2c32d659ff2bab9799cc5de14b2e
MaryPoddubnaya <mary.poddubnaya@teamdev.com>
2017-05-25 11:10:44 +0300

    Rename images

--- dev/js/main.js
+++ dev/js/main.js
@@ -7,33 +7,30 @@ $(document).ready(function() {
     });
 
     //header animation
-        var shrinkHeader = 300;
-        $(window).scroll(function() {
-            var scroll = getCurrentScroll();
-            if ( scroll >= shrinkHeader ) {
-                $('.header').addClass('shrink');
-            }
-            else {
-                $('.header').removeClass('shrink');
-            }
-        });
-        function getCurrentScroll() {
-            return window.pageYOffset || document.documentElement.scrollTop;
+    $(window).on('resize scroll', function() {
+        var width = $(document).width();
+
+        if ($(window).scrollTop()) {
+            $('header').addClass('shrink');
         }
+        else {
+            $('header').removeClass('shrink');
+        }
+        if (width < 768) {
+            $('header').removeClass('shrink');
+        }
+    });
 
     // Get the height of the header
     var headerHeight = $("header").height();
 
     $(".flex-nav .flex-item-list").find("a").bind("click", function(e) {
-
         e.preventDefault();
-        var target = $(this).attr("href"); //Get the target
+        var target = $(this).attr("href");
         var scrollToPosition = $(target).offset().top - headerHeight;
 
         $("html, body").animate({ 'scrollTop': scrollToPosition }, 900, function(){
             window.location.hash = "" + target;
-            // This hash change will jump the page to the top of the div with the same id
-            // so we need to force the page to back to the end of the animation
             $("html, body").animate({ 'scrollTop': scrollToPosition }, 0);
         });
 
